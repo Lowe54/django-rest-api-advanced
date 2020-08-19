2540ea6660d2fc1b16dff935975074aa56419cc1
James Lowe <jameslowe241@gmail.com>
2020-08-19 18:09:52 +0100

    Removed un-needed permissions file

--- app/user/permissions.py
+++
@@ -1,9 +0,0 @@
-# from rest_framework.permissions import BasePermission, SAFE_METHODS
-
-
-# class IsOwnerProfile(BasePermission):
-#     def has_object_permission(self, request, view, obj):
-#         if request.method in SAFE_METHODS:
-#             return True
-#         if request.user.id is not None:
-#             return obj.user == request.user
