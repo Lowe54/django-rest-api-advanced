6181b2fddff967036c12ab564e2935ebc65902f1
James Lowe <jameslowe241@gmail.com>
2020-08-23 14:05:22 +0100

    Added create ingredient tests

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -67,3 +67,26 @@ class PrivateIngredientsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], New_Ingredient.name)
+
+    def test_create_ingredient_successful(self):
+        """Test that the tag can be created"""
+        payload = {
+            'name': 'Test Ingredient',
+
+        }
+        self.client.post(INGREDIENTS_URL, payload)
+
+        exists = Ingredient.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+
+        self.assertTrue(exists)
+
+    def test_tag_valid(self):
+        """ Test creating a new ingredient with an empty payload"""
+        payload = {'name': ''}
+
+        res = self.client.post(INGREDIENTS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
