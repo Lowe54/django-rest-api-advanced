7c9baa50df9a1033aba6d6e5f8d61e250585c88e
James Lowe <jameslowe241@gmail.com>
2020-08-23 12:42:11 +0100

    Tag management added

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins, permissions
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+from core.models import Tag
+from recipe import serializers
+
+
+class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """Manage Tags in the database"""
+
+    authentication_classes = (JWTAuthentication, )
+    permission_classes = (permissions.IsAuthenticated, )
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user"""
+
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+    
